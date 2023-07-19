573fa2b4434f1d98e4070cf3fa8e0aa253ef7a1b
camiloruizvidal <camiloruizvidal@gmail.com>
2023-07-18 21:40:41 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import * as cors from 'cors';
 // create express app
 const app = express()
 
+const staticPort = 3050;
+const port = process.env.PORT || staticPort;
+console.log({port});
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
