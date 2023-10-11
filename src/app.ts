db3b05ff8733ff2e55ae8d69b1da5d7235a9d057
camiloruiz <camiloruizvidal@gmail.com>
2023-10-11 11:58:42 -0500

    deleted cors

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,6 @@ const app = express()
 
 const port = process.env.PORT || 3050;
 
-// Configuración de CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
