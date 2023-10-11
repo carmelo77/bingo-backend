722ea0e06e18b2a2469d511c189f105fabc59003
root <root@vps.server.local>
2023-10-11 15:44:25 +0000

    Descripción de los cambios

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from 'cors';
+import * as os from "os";
 
 // create express app
 const app = express()
 
 const staticPort = 3050;
 const port = process.env.PORT || staticPort;
+const serverIP = Object.values(os.networkInterfaces())
+  .map((interfaces: any) =>
+    interfaces.find(
+      (iface) => iface.family === "IPv4" && !iface.internal
+    )
+  )
+  .find((iface) => iface !== undefined)?.address;
+
+console.log("Server IP:", serverIP);
 console.log({port});
 
 app.use(bodyParser.json());
